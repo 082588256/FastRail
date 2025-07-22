81dcd7a5f225092ddc57f766c94098a90d877822
082588256 <daipthe160387@fpt.edu.vn>
2025-07-22 10:20:12 +0700

    update client

--- Project/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Project/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9e96211efbb3e705f66c981e41df2c74dff0b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee95f2f5b409eb975ad47b166d6c6dbc95c8566")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
