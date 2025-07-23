747fbad01e6ec3b69fbf013ed8493b0b452da800
lht <tuanlhhe163470@fpt.edu.vn>
2025-07-23 10:53:23 +0700

    add ticket func

---
+++ Project/ProjectView/Controllers/FareController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectView.Controllers
+{
+    public class FareController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View(); // Trả về Views/Fare/Index.cshtml
+        }
+    }
+}
